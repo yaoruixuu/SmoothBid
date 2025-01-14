d7bd141a97b6dafe9ef69e50a4eb9341c1e7c9fd
Yaorui <yaoruixuu@gmail.com>
2025-01-14 18:31:41 -0500

    fonts

--- commerce/settings.py
+++ commerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['yaoruixuu.pythonanywhere.com']
+ALLOWED_HOSTS = ['yaoruixuu.pythonanywhere.com', '127.0.0.1']
 
 
 
