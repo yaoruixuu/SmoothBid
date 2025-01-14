f95971d3cdc7e38e37ce8fbdd7d7fe71d065ec4f
Yaorui <yaoruixuu@gmail.com>
2025-01-14 10:50:27 -0500

    fixed path for static

--- commerce/settings.py
+++ commerce/settings.py
@@ -125,5 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = BASE_DIR / 'productionfiles'
 
